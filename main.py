2510fb8459f0b8a13076f8071b4e75d2305c379d
kateryna.kochina <kochina.ev@gmail.com>
2023-11-13 15:56:34 -0500

    Initial commit for portfolio website

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
