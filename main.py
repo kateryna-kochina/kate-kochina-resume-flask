f4bd407a5cc4a796bb110ecca3ba01a2a1ceb958
kateryna.kochina <kochina.ev@gmail.com>
2023-11-14 15:03:48 -0500

    Add contact options

--- main.py
+++ main.py
@@ -11,5 +11,11 @@ def home():
     return render_template('index.html')
 
 
+# additional contact form
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
